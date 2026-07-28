121459f039d9c9e2bf9908fb32dedc8269a5f523
su2u4 <Joey23.su@gmail.com>
2026-07-29 01:36:08 +0800

    update bot.bat

--- bot.bat
+++ bot.bat
@@ -3,14 +3,16 @@ set REMOTE_USER=su2u4
 set REMOTE_IP=192.168.0.97
 set REMOTE_SCRIPT=~/bot.sh
 
-if "%~1"=="" (
-    echo 使用方式: bot.bat {start^|stop^|restart^|status^|log}
-    echo   bot.bat start   - 遠端啟動 Bot
-    echo   bot.bat stop    - 遠端關閉 Bot
-    echo   bot.bat restart - 遠端重啟 Bot
-    echo   bot.bat status  - 查看遠端運行狀態
-    echo   bot.bat log     - 查看即時 Log
+if "%1"=="" (
+    echo 使用方式: bot.bat {start|stop|restart|status|log}
     exit /b 1
 )
 
-ssh -t %REMOTE_USER%@%REMOTE_IP% "%REMOTE_SCRIPT% %1"
+:: 如果是觀看 Log，需要保留互動式 TTY
+if "%1"=="log" (
+    ssh -t su2u4@192.168.0.97 "/home/su2u4/bot.sh log"
+    exit /b 0
+)
+
+:: 其他指令（start/stop/restart/status）：重定向 stdin，讓 SSH 完成後安全斷開，不咬住背景進程
+ssh su2u4@192.168.0.97 "/home/su2u4/bot.sh %1 < /dev/null"
